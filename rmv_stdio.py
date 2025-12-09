c837a910d8aed7e580df5d4e6fbe1e4e9d6bcfa9
Volker Benders <39129892+CWACoderWithAttitude@users.noreply.github.com>
2025-12-09 10:39:09 +0100

    Change transport method to 'http' in main function

--- rmv_stdio.py
+++ rmv_stdio.py
@@ -55,4 +55,4 @@ async def get_connections(
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
